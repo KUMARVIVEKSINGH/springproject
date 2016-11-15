2dc58784de8a9c8418ccc5df2cb697626cf6330a
kumarviveksingh <kumarviveksingh@gmail.com>
2016-11-15 15:10:13 +0530

    Added logger

--- src/main/java/springproject/MainApp.java
+++ src/main/java/springproject/MainApp.java
@@ -1,13 +1,16 @@
 package springproject;
 
+import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MainApp {
+    private static Logger log = Logger.getLogger(MainApp.class);
+
     public static void main(String[] args) {
         ApplicationContext ctx = new AnnotationConfigApplicationContext(
                 HelloWorldConfiguration.class);
         HelloWorld helloWorld = ctx.getBean(HelloWorld.class);
-        System.out.println(helloWorld.getMessage().toString());
+        log.debug(helloWorld.getMessage().toString());
     }
 }
